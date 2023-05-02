61c68cc0d3149e61ccb3ef26ad519ffe31db26e8
Futar0 <104120854+Futar0@users.noreply.github.com>
2023-05-02 08:42:46 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch lineage_certus-userdebug
+export SKIP_API_CHECKS=true
+export SKIP_ABI_CHECKS=true
 export TZ=Asia/Jakarta #put before last build command
 make bacon
 
