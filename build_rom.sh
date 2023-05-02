ae8cbeb2350630cd194e2427f1007169da6180af
Uzui Tengen <121469941+Uzui01@users.noreply.github.com>
2023-05-02 14:30:50 +0800

    Lineage19-1

--- build_rom.sh
+++ build_rom.sh
@@ -9,7 +9,7 @@ lunch lineage_certus-userdebug
 export SKIP_API_CHECKS=true
 export SKIP_ABI_CHECKS=true
 export TZ=Asia/Jakarta #put before last build command
-make bacon -j32
+make bacon -j16
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
